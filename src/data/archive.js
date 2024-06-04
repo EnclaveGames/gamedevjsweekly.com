5deb66fcb08be0ffd146d571c6d0c24fc88d5f6b
Michał Chojnacki <m.chojnacki@muyo.io>
2024-06-04 22:32:52 +0200

    Simplify archive fetches and uses
    One globally accessible array does the trick - no need to wrap all access in async calls.

---
+++ src/data/archive.js
@@ -0,0 +1,59 @@
+function dtf(time) {
+  return time.getFullYear()
+    + '/'
+    + ('00' + (time.getMonth() + 1)).slice(-2)
+    + '/'
+    + ('00' + time.getDate()).slice(-2)
+}
+
+const
+  cache = [],
+  res   = await fetch('https://us3.api.mailchimp.com/3.0/campaigns?folder_id=73571a9f27&count=1000&sort_field=send_time&fields=campaigns.id,campaigns.send_time,campaigns.settings.subject_line,campaigns.social_card.image_url', {
+  headers: {
+    Authorization: import.meta.env.MAILCHIMP_AUTH
+  }
+})
+
+if (!res.ok) {
+  throw new Error(res.status + ': ' + await res.text())
+}
+
+const src = (await res.json()).campaigns
+for (let i = 0; i < src.length; i++) {
+  const
+    v   = src[i],
+    idx = i + 1
+
+  let title = ''
+  if (idx >= 241) {
+    title = v.settings.subject_line.replace(/^(.*?): /gi, '')
+
+    // Naive but simple enough to avoid a re-allocation (and regex) just to compare a letter.
+    // In case the first  char is not ASCII this will not work, of course, but does the trick
+    // otherwise.
+    //
+    // TODO(alcore) Ideally we'd have this fixed straight at the data source, but data and content
+    // consistency is not Ender's strong suit...
+    let c = title.charCodeAt(0)
+    if (c > 90) {
+      title = title[0].toUpperCase() + title.slice(1)
+    }
+  }
+
+  // Insert in reverse order (i.e. newest first)
+  cache[src.length - idx] = {
+    i:     idx,
+    id:    v.id,
+    title: title ? title : 'Gamedev.js Weekly #' + idx,
+    // Note: Not all issues have a OG image. Some of the earliest ones have none at all, while many afterward
+    // reuse a common "header image" instead of a proper, dedicated image.
+    image: v.social_card?.image_url,
+    // Note: Those are ISO 8601, so no need to process them in any other way than for display.
+    // TODO(alcore) However, a nicer display format would be welcome (and whether this should be precomputed at
+    // all is a different question).
+    dt:    v.send_time,
+    at:    dtf(new Date(v.send_time))
+  }
+}
+
+export default cache
