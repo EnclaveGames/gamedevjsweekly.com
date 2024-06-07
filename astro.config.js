e1e50b2b84eb64f9627e5844e922d701f8d97e76
alcore <m.chojnacki@muyo.io>
2024-06-07 21:40:38 +0200

    Aggressively minify build artifacts
    tl;dr: Some unsafe, slow and extremely inefficient post processing shaves off an extra 10-20% bytes per page.
    
    The optimizations are usually unsafe, but this is a simple enough site to manage. The "Astro" approach to this is insanely wasteful and inefficient, due to everything being processed many times over - and after bundle completion at that. However, the built static HTML is not accessible prior to that step.
    
    At the very least it should be possible to decompose this and move the additional 'clean-css' post-processing step (which shaves off a few extra bytes on top of lightningcss - albeit unsafely as well) into a Vite plugin before it gets taken over by Astro and inserted, hundreds of times per build. Silly.
    
    This also needed issue 509 to get a static override, due to the unescaped `<$1,000` returned from Mailchimp. It needs to be `&lt;$1,000` so that `html-minifier` doesn't choke on an unclosed opening tag.

--- astro.config.js
+++ astro.config.js
@@ -1,9 +1,14 @@
+import fs from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+
 import { defineConfig } from 'astro/config'
+import { minify }       from 'html-minifier-terser'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gamedevjsweekly.com',
   trailingSlash: 'never',
+  compressHTML: false,
   build: {
     format: 'file'
   },
@@ -11,5 +16,42 @@ export default defineConfig({
     build: {
       cssMinify: 'lightningcss'
     }
-  }
+  },
+  integrations: [{
+    name: 'gdjs:post:minify',
+    hooks: {
+      'astro:build:done': ({ dir, pages }) => Promise.all(pages.map(v => {
+        const
+          s = v.pathname || 'index',
+          p = fileURLToPath(new URL(s + '.html', dir))
+
+        return fs.readFile(p)
+          .then(v => minify(v.toString(), {
+            caseSensitive:                 true,
+            keepClosingSlash:              true,
+            collapseBooleanAttributes:     true,
+            collapseInlineTagWhitespace:   true,
+            collapseWhitespace:            true,
+            minifyURLs:                    true,
+            quoteCharacter:                "'",
+            removeOptionalTags:            true,
+            removeAttributeQuotes:         true,
+            removeEmptyAttributes:         true,
+            removeRedundantAttributes:     true,
+            removeScriptTypeAttributes:    true,
+            removeStyleLinkTypeAttributes: true,
+            removeEmptyElements:           true,
+            removeComments:                true,
+            minifyJS:                      false,
+            minifyCSS:                     {
+              level: {
+                1: { all: true },
+                2: { all: true }
+              }
+            },
+          }))
+        .then(v => fs.writeFile(p, v))
+      }))
+    }
+  }]
 })
